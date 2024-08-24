d2b74fd3a8caf2339488b75853ffce9b32a138a6
Divyansh Sinha <harshsinha190@gmail.com>
2024-08-25 01:02:49 +0530

    Database link added

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,8 @@
 import { SignupValidation } from "@/lib/validation"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
-import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form"
+import { useToast } from "@/components/ui/use-toast"
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form"
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
@@ -14,7 +15,9 @@ import { createUserAccount } from "@/lib/appwrite/api"
 
 
 
+
 const SignupForm = () => {
+  const {toast} = useToast();
   const isLoading = false;
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -30,7 +33,13 @@ const SignupForm = () => {
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
     const newUser = await createUserAccount(values);
-    console.log(newUser);
+    
+    if(!newUser)
+    {
+      return toast({title: "Sign Up Failed! Please try again."})
+    }
+
+    // const session = await signInAccount()
   }
 
   return (
